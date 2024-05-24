6507369962e50d415524160e127be1851e692af2
vlambo3 <contadora.godoy.vanina@gmail.com>
2024-05-24 10:54:12 -0300

    firt commit

---
+++ src/main/java/com/dh/TaskManager/entity/Task.java
@@ -0,0 +1,34 @@
+package com.dh.TaskManager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Representa una tarea en el sistema.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    /**
+     * El identificador único de la tarea.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * El título de la tarea.
+     */
+    private String title;
+
+    /**
+     * El estado de la tarea.
+     */
+    private Boolean completed = false;
+
+
+}
