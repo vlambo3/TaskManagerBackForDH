e900cea5b0125ebfca85d6d28c0a6ad1017a10f2
Vanina Godoy <86501009+vlambo3@users.noreply.github.com>
2024-05-28 14:50:21 -0300

    Update WebConfig.java

--- src/main/java/com/dh/TaskManager/configuration/WebConfig.java
+++ src/main/java/com/dh/TaskManager/configuration/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedOrigins("....")
+                .allowedOrigins("*")
+                //.allowedOrigins("....")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
